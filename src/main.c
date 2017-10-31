1a8c76eef56a599c07e4bb2414d004e6bb340ba3
gertve <gert.vesterberg@gmail.com>
2017-10-31 23:16:22 +0200

    RGB blink led

--- src/main.c
+++ src/main.c
@@ -1,21 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 500
 
-void main (void)
+void blink (int port)
 {
-    DDRB |= (_BV(DDB5)) | (_BV(DDB4));
+    PORTA |= port;
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~port;
+    _delay_ms(BLINK_DELAY_MS);
+}
+
+void main (void)  {
+    DDRB |= _BV(DDB7);
+    DDRA |= (_BV(DDA0)) | (_BV(DDA2)) | (_BV(DDA4));
+    PORTB &= ~_BV(PORTB7);
 
     while (1) {
-        PORTB |= _BV(PORTB5);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTB &= ~_BV(PORTB5);
-        _delay_ms(BLINK_DELAY_MS);
-
-        PORTB |= _BV(PORTB4);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTB &= ~_BV(PORTB4);
-        _delay_ms(BLINK_DELAY_MS);
+        blink(_BV(PORTA0));
+        blink(_BV(PORTA2));
+        blink(_BV(PORTA4));
     }
 }
